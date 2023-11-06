6f1db53b64e224d6e5d5debe0d931f1c06ba25f5
Anchon23 <angellopezrouanet@gmail.com>
2023-11-06 13:29:18 +0100

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+def mover_piedras(n, origen, destino, auxiliar):
+    if n == 1:
+        print(f"Mover piedra {n} desde {origen} a {destino}")
+    else:
+        mover_piedras(n-1, origen, auxiliar, destino)
+        print(f"Mover piedra {n} desde {origen} a {destino}")
+        mover_piedras(n-1, auxiliar, destino, origen)
+
+mover_piedras(74, "x", "y", "z")
